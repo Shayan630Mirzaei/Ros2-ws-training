8be679963ac4f5dc78709129549f4049ef310d8a
Shayan630 <hamidreza630.mirzaei@gmail.com>
2026-04-20 09:13:40 -0400

    HW Interface Has started

---
+++ src/two_wheels_robot_hardware/include/two_wheels_robot_hardware/mobile_base_hardware_interface.hpp
@@ -0,0 +1,35 @@
+#ifndef MOBILE_BASE_HARDWARE_INTERFACE_HPP
+#define MOBILE_BASE_HARDWARE_INTERFACE_HPP
+
+
+#include "hardware_interface/system_interface.hpp"
+#include "two_wheels_robot_hardware/xl330_driver.hpp"    // for this line, you should update json file by adding directroy of two_wheels_robot_hardware 
+
+namespace mobile_base_hardware{
+
+    class Mobile_base_hw_itf : public hardware_interface :: SystemInterface
+    {
+        public:
+            // lifecycle node override
+            hardware_interface::CallbackReturn
+                on_configure(const rclcpp_lifecycle::State &previous_state) override;
+
+            hardware_interface::CallbackReturn
+                on_activate(const rclcpp_lifecycle::State &previous_state) override;
+
+            hardware_interface::CallbackReturn
+                on_deactivate(const rclcpp_lifecycle::State &previous_state) override;
+
+            // System Interface override
+
+        private:
+
+
+
+
+    }; //  class Mobile_base_hw_itf
+
+} //  namespace mobile_base_hardware
+
+
+#endif 
