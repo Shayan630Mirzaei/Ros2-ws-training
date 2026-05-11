fae36955cd535a877ca8cc4bb8a905b21ffc6099
Shayan630 <hamidreza630.mirzaei@gmail.com>
2026-05-11 08:47:47 -0400

    HW Interfaces is finished and debugged.Bias is removed, and hard code paramteres are updated

--- src/two_wheels_robot_hardware/include/two_wheels_robot_hardware/mobile_base_hardware_interface.hpp
+++ src/two_wheels_robot_hardware/include/two_wheels_robot_hardware/mobile_base_hardware_interface.hpp
@@ -1,42 +1,46 @@
 #ifndef MOBILE_BASE_HARDWARE_INTERFACE_HPP
 #define MOBILE_BASE_HARDWARE_INTERFACE_HPP
 
-
 #include "hardware_interface/system_interface.hpp"
-#include "two_wheels_robot_hardware/xl330_driver.hpp"    // for this line, you should update json file by adding directroy of two_wheels_robot_hardware 
+#include "hardware_interface/types/hardware_interface_type_values.hpp"
+#include "two_wheels_robot_hardware/xl330_driver.hpp"
 
-namespace mobile_base_hardware{
+namespace mobile_base_hardware
+{
 
-    class Mobile_base_hw_itf : public hardware_interface::SystemInterface
-    {
-        public:
-            // lifecycle node override
-            hardware_interface::CallbackReturn
-                on_configure(const rclcpp_lifecycle::State &previous_state) override;
+class Mobile_base_hw_itf : public hardware_interface::SystemInterface
+{
+public:
+    // lifecycle callbacks override
+    hardware_interface::CallbackReturn on_init(const hardware_interface::HardwareComponentInterfaceParams & params) override;
+    hardware_interface::CallbackReturn on_configure(const rclcpp_lifecycle::State & previous_state) override;
+    hardware_interface::CallbackReturn on_activate(const rclcpp_lifecycle::State & previous_state) override;
+    hardware_interface::CallbackReturn on_deactivate(const rclcpp_lifecycle::State & previous_state) override;
 
-            hardware_interface::CallbackReturn
-                on_activate(const rclcpp_lifecycle::State &previous_state) override;
+    // export interfaces
+    std::vector<hardware_interface::StateInterface>export_state_interfaces() override;
+    std::vector<hardware_interface::CommandInterface>export_command_interfaces() override;
 
-            hardware_interface::CallbackReturn
-                on_deactivate(const rclcpp_lifecycle::State &previous_state) override;
+    // read/write
+    hardware_interface::return_type read(const rclcpp::Time & time,const rclcpp::Duration & period) override;
+    hardware_interface::return_type write(const rclcpp::Time & time,const rclcpp::Duration & period) override;
 
-            // System Interface override
-            hardware_interface::CallbackReturn
-                on_init(const hardware_interface::HardwareComponentInterfaceParams & params) override;
-            hardware_interface::return_type
-                read(const rclcpp::Time &time, const rclcpp::Duration &period);
-            hardware_interface::return_type
-                write(const rclcpp::Time &time, const rclcpp::Duration &period);
-       
-        private:
-            std::shared_ptr<XL330Driver> driver_;
-            int left_motor_id_;
-            int right_motor_id_;
-            std::string port_;
+private:
+    std::shared_ptr<XL330Driver> driver_;
+    int left_motor_id_;
+    int right_motor_id_;
+    std::string port_;
 
-    }; //  class Mobile_base_hw_itf
+    double left_position_  = 0.0;
+    double right_position_ = 0.0;
 
-} //  namespace mobile_base_hardware
+    double left_velocity_  = 0.0;
+    double right_velocity_ = 0.0;
 
+    double left_command_   = 0.0;
+    double right_command_  = 0.0;
+};
 
-#endif 
+} // namespace mobile_base_hardware
+
+#endif
--- src/two_wheels_robot_hardware/src/mobile_base_hardware_interface.cpp
+++ src/two_wheels_robot_hardware/src/mobile_base_hardware_interface.cpp
@@ -1,97 +1,131 @@
-#include<two_wheels_robot_hardware/mobile_base_hardware_interface.hpp>  // first we built header in include folder in this package,
-                                                                        // then we call it here
+#include <two_wheels_robot_hardware/mobile_base_hardware_interface.hpp>
 
 #include "hardware_interface/system_interface.hpp"
-namespace mobile_base_hardware{
+
+namespace mobile_base_hardware
+{
 
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    hardware_interface::CallbackReturn
-    Mobile_base_hw_itf::on_init(
-    const hardware_interface::HardwareComponentInterfaceParams & params)
+hardware_interface::CallbackReturn Mobile_base_hw_itf::on_init(const hardware_interface::HardwareComponentInterfaceParams & params)
 {
     info_ = params.hardware_info;
 
-    left_motor_id_  = 10;
-    right_motor_id_ = 20;
-    port_           = "/dev/ttyACM0";
+    left_motor_id_  = std::stoi(info_.hardware_parameters["left_motor_id"]);
+    right_motor_id_ =  std::stoi(info_.hardware_parameters["right_motor_id"]);;
+    port_           = info_.hardware_parameters["dynamixel_port"];  // "/dev/ttyACM0"
 
-    driver_ = std::make_shared<XL330Driver>(port_);
+    driver_         = std::make_shared<XL330Driver>(port_);
 
     return hardware_interface::CallbackReturn::SUCCESS;
 }
 
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    hardware_interface::CallbackReturn Mobile_base_hw_itf::on_configure(const rclcpp_lifecycle::State &previous_state)
+hardware_interface::CallbackReturn Mobile_base_hw_itf::on_configure(const rclcpp_lifecycle::State & previous_state)
+{
+    (void)previous_state;
+
+    if (driver_->init() != 0)
     {
-        (void)previous_state;
-        if (driver_->init() != 0)
-        {
-            return hardware_interface::CallbackReturn::ERROR;
-        }
-
-        return hardware_interface::CallbackReturn::SUCCESS;  
-     }
-
-// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    hardware_interface::CallbackReturn  Mobile_base_hw_itf::on_activate(const rclcpp_lifecycle::State &previous_state)
-     {
-        (void)previous_state;
-        set_state("base_left_wheel_joint/velocity",  0.0);
-        set_state("base_right_wheel_joint/velocity", 0.0);
-        set_state("base_left_wheel_joint/position",  0.0);
-        set_state("base_right_wheel_joint/position", 0.0);
-
-        driver_->activateWithVelocityMode(left_motor_id_);
-        driver_->activateWithVelocityMode(right_motor_id_);
-        return hardware_interface::CallbackReturn::SUCCESS;  
-     }
-
-// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    hardware_interface::CallbackReturn Mobile_base_hw_itf::on_deactivate(const rclcpp_lifecycle::State &previous_state)
-    {
-        (void)previous_state;
-       driver_->deactivate(left_motor_id_);
-       driver_->deactivate(right_motor_id_);
-       return hardware_interface::CallbackReturn::SUCCESS;  
-
+        return hardware_interface::CallbackReturn::ERROR;
     }
 
 
+    return hardware_interface::CallbackReturn::SUCCESS;
+}
+
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+hardware_interface::CallbackReturn Mobile_base_hw_itf::on_activate(const rclcpp_lifecycle::State & previous_state)
+{
+    (void)previous_state;
+
+    left_position_  = 0.0;
+    right_position_ = 0.0;
+
+    left_velocity_  = 0.0;
+    right_velocity_ = 0.0;
+
+    left_command_   = 0.0;
+    right_command_  = 0.0;
+
+    driver_->activateWithVelocityMode(left_motor_id_);
+    driver_->activateWithVelocityMode(right_motor_id_);
+
+    return hardware_interface::CallbackReturn::SUCCESS;
+}
+
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    hardware_interface::return_type Mobile_base_hw_itf::read(const rclcpp::Time &time, const rclcpp::Duration &period)
-    {
-        (void)time;
-        double left_vel  = driver_->getVelocityRadianPerSec(left_motor_id_);
-        double right_vel = driver_->getVelocityRadianPerSec(right_motor_id_);
+hardware_interface::CallbackReturn Mobile_base_hw_itf::on_deactivate(const rclcpp_lifecycle::State & previous_state)
+{
+    (void)previous_state;
 
-        set_state("base_left_wheel_joint/velocity", left_vel);
-        set_state("base_right_wheel_joint/velocity", right_vel);
+    driver_->deactivate(left_motor_id_);
+    driver_->deactivate(right_motor_id_);
 
-        set_state("base_left_wheel_joint/position",  get_state("base_left_wheel_joint/position")  + left_vel * period.seconds());
-        set_state("base_right_wheel_joint/position", get_state("base_right_wheel_joint/position") + right_vel * period.seconds());
+    return hardware_interface::CallbackReturn::SUCCESS;
+}
 
-        return hardware_interface::return_type::OK; 
-
-    }
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+std::vector<hardware_interface::StateInterface>Mobile_base_hw_itf::export_state_interfaces()
+{
+    std::vector<hardware_interface::StateInterface> state_interfaces;
+
+    state_interfaces.emplace_back("base_left_wheel_joint","position",&left_position_);
+    state_interfaces.emplace_back("base_left_wheel_joint","velocity",&left_velocity_);
+
+    state_interfaces.emplace_back("base_right_wheel_joint","position",&right_position_);
+    state_interfaces.emplace_back("base_right_wheel_joint","velocity",&right_velocity_);
+
+    return state_interfaces;
+}
+
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    hardware_interface::return_type Mobile_base_hw_itf::write(const rclcpp::Time &time, const rclcpp::Duration &period)
-    {
-        (void)time;
-        (void)period;
-        driver_->setTargetVelocityRadianPerSec(left_motor_id_ , get_command("base_left_wheel_joint/velocity"));
-        driver_->setTargetVelocityRadianPerSec(right_motor_id_, get_command("base_right_wheel_joint/velocity"));
-        return hardware_interface::return_type::OK; 
-        
-    }
+std::vector<hardware_interface::CommandInterface>Mobile_base_hw_itf::export_command_interfaces()
+{
+    std::vector<hardware_interface::CommandInterface> command_interfaces;
+    command_interfaces.emplace_back("base_left_wheel_joint","velocity",&left_command_);
+    command_interfaces.emplace_back("base_right_wheel_joint","velocity",&right_command_);
 
+    return command_interfaces;
+}
 
-}    
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+hardware_interface::return_type Mobile_base_hw_itf::read(const rclcpp::Time & time,const rclcpp::Duration & period)
+{
+    (void)time;
+
+    left_velocity_  = driver_->getVelocityRadianPerSec(left_motor_id_);
+    right_velocity_ = driver_->getVelocityRadianPerSec(right_motor_id_) * -1.0;
+
+    if (abs(left_velocity_)<0.03){left_velocity_=0;};    // compensating inaccuracy of motors
+    if (abs(right_velocity_)<0.03){right_velocity_=0;};
+
+    left_position_ += left_velocity_ * period.seconds();
+    right_position_ += right_velocity_ * period.seconds();
+
+    RCLCPP_INFO(get_logger(),"left com: %lf",left_command_);
+    RCLCPP_INFO(get_logger(),"left vel: %lf",left_velocity_);
+    //RCLCPP_INFO(get_logger(),"left pos: %lf",left_position_);
+    RCLCPP_INFO(get_logger(),"right com: %lf",right_command_);
+    RCLCPP_INFO(get_logger(),"right vel: %lf",right_velocity_);
+    return hardware_interface::return_type::OK;
+}
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+hardware_interface::return_type Mobile_base_hw_itf::write(const rclcpp::Time & time, const rclcpp::Duration & period)
+{
+    (void)time;
+    (void)period;
+
+    driver_->setTargetVelocityRadianPerSec(left_motor_id_,left_command_);
+    driver_->setTargetVelocityRadianPerSec(right_motor_id_,-1.0 * right_command_); // -1 is due to two motors shout have oposit direction, otherwise robot don't move forward
+       
+    return hardware_interface::return_type::OK;
+}
+
+} // namespace mobile_base_hardware
 
 #include "pluginlib/class_list_macros.hpp"
-PLUGINLIB_EXPORT_CLASS(mobile_base_hardware::Mobile_base_hw_itf , hardware_interface::SystemInterface)
+
+PLUGINLIB_EXPORT_CLASS(
+    mobile_base_hardware::Mobile_base_hw_itf,
+    hardware_interface::SystemInterface)
